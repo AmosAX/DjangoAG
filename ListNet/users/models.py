77e6a91c84e468e5b243e486ba785e106daf5771
AmosAX <77676366+AmosAX@users.noreply.github.com>
2023-10-31 23:43:57 +0200

    nu borde user uploads fungera

--- ListNet/users/models.py
+++ ListNet/users/models.py
@@ -10,10 +10,11 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     # Create your models here.
-    def save(self):
-        super().save()
+    
+    #konstig save bug lägg till detta 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
-
         if img.height > 300 or img.width > 300:
             output_size = (300,300)
             img.thumbnail(output_size)
